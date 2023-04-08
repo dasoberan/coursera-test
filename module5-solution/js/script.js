87a1925bdc873237a069a2edcb2b8d5f88742d93
dsoberan <dsoberan@masonlive.gmu.edu>
2023-04-08 16:11:47 -0400

    Module 5 Coding Assignment UPDATED

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -122,7 +122,7 @@ function buildAndShowHomeHTML (categories) {
       // TODO: STEP 4: Insert the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
-      insertHTML("#main-page", homeHtmlToInsertIntoMainPage);
+      //insertHTML("#main-page", homeHtmlToInsertIntoMainPage);
 
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
